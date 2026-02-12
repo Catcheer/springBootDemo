9ae4c959630194419b28d17254d5c4bee7f8e9d7
catcheer <1072491557@qq.com>
2026-02-12 09:35:45 +0800

    student

--- src/main/java/com/example/springbootdemo/mapper/StudentServiceMapper.java
+++ src/main/java/com/example/springbootdemo/mapper/StudentServiceMapper.java
@@ -8,27 +8,31 @@ import java.util.List;
 @Mapper
 public interface StudentServiceMapper {
 
-    @Select("""
-                SELECT * FROM `user`;
-            """)
-    public List<Student> findAll();
+        @Select("""
+                            SELECT * FROM `student`;
+                        """)
+        public List<Student> findAll();
 
-    @Insert("""
-            INSERT INTO `user`
-            (id,name,email,password)
-            VALUES(#{id},#{name},#{email},#{password});
-            """)
-    public void addStudent(Student student);
+        @Insert("""
+                        INSERT INTO `student`
+                        (student_no,name,gender,birthday,phone,class_id)
+                        VALUES(#{student_no},#{name},#{gender},#{birthday},#{phone},#{class_id});
+                        """)
+        public void addStudent(Student student);
 
-    @Update("""
-            UPDATE `user`
-            SET name=#{name}
-            WHERE id=#{id}
-            """)
-    public  void updateStudent(Student student);
+        @Update("""
+                        UPDATE `student`
+                        SET name=#{name}
+                        SET gender=#{gender}
+                        SET birthday=#{birthday}
+                        SET phone=#{phone}
+                        SET class_id=#{class_id}
+                        WHERE id=#{id}
+                        """)
+        public void updateStudent(Student student);
 
-    @Delete("""
-            DELETE FROM `user` WHERE id=#{id}
-            """)
-    void delStudent(String id);
+        @Delete("""
+                        DELETE FROM `student` WHERE id=#{id}
+                        """)
+        void delStudent(String id);
 }
--- src/main/java/com/example/springbootdemo/model/Student.java
+++ src/main/java/com/example/springbootdemo/model/Student.java
@@ -1,58 +1,26 @@
 package com.example.springbootdemo.model;
 
+import java.time.LocalDateTime;
 
-//import jakarta.persistence.*;
+import lombok.Data;
 
+// import jakarta.persistence.*;
 
 //@Entity
-//@Table(name = "user") // 对应数据库里的 student 表
-public class Student   {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY) // 主键自增
-    private  int id;
-    private  String name;
-    private String email;
-    private String password;
+// @Table(name = "student") // 对应数据库里的 student 表
+@Data
+public class Student {
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY) // 主键自增
+    // @GeneratedValue(strategy = GenerationType.IDENTITY) // 主键自增
+    private int id;
+    private String name;
+    private String gender;
+    private String birthday;
+    private String phone;
+    private int class_id;
+    private String studentNo;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 
-
-    public  Student(){}
-
-    public Student(int id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
