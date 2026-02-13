b2c001854566dee7d7143845754d1a31f5d09213
catcheer <1072491557@qq.com>
2026-02-13 20:16:59 +0800

    locationXml

--- src/test/java/com/example/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/example/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,14 +1,21 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.dto.StudentQuery;
+import com.example.springbootdemo.mapper.StudentServiceMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 
 class SpringBootDemoApplicationTests {
 
+  @Autowired
+  private StudentServiceMapper studentServiceMapper;
+
   @Test
-  public void test1(){
-      System.out.println("test1");
+  public void test1() {
+    System.out.println("test1");
+    studentServiceMapper.getStudents(new StudentQuery(), 0, 10);
   }
 }
